0eca20e54a9a25780f6469117d60b42229b430f1
Alice Wilson <alice.g.wilson@hotmail.co.uk>
2024-09-19 17:01:17 +0100

    styling landing page

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -1,7 +1,7 @@
 import { IoMdStar } from "react-icons/io";
 
 export default function TestimonialCard(props) {
-    const starStyle = {color: '#AA8D7C', fontSize: '3rem'}
+    const starStyle = {color: '#AA8D7C', fontSize: '2.5rem', padding: 0, margin: 0}
     return (
         <div className="testimonial-card">
             <div className="stars"><IoMdStar style = {starStyle}/><IoMdStar style = {starStyle}/><IoMdStar style = {starStyle}/><IoMdStar style = {starStyle}/><IoMdStar style = {starStyle}/></div>
